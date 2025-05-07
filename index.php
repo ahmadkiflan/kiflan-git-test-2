d4c1378ec4a1181954dc5e4f42c9d27f742dae8c
ahmadkiflan <ahmadkiflan2020@gmail.com>
2025-05-07 15:47:19 +0700

    Menambahkan list baru ke file index.php

--- index.php
+++ index.php
@@ -10,6 +10,10 @@
 <body>
     <h1>Hello, World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia nemo voluptate facere deleniti, aliquid debitis aliquam eius molestias eligendi unde sit possimus consectetur veritatis cumque illo exercitationem veniam numquam dolor.</p>
+    <ul>
+        <li>Nama : Ahmad Kiflan Wafi</li>
+        <li>NIM : B0522003</li>
+    </ul>
 </body>
 
 </html>
